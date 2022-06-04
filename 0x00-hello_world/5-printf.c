3101f64838e9b4dcc64cef8c366a0734c4a2d1e8
Aletia88 <abababmgmgm@gmail.com>
2022-06-04 06:04:37 -0700

    print f

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,9 +2,11 @@
 #include <stdlib.h>
 /**
  *main - print the text
+ *
+ *return: 0
  */
 int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art, ");
-  return (0);
+printf("with proper grammar, but the outcome is a piece of art, ");
+return (0);
 }
