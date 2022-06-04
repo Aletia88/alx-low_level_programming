199c58650518faa707932a4a73790360e4fc5307
Aletia88 <abababmgmgm@gmail.com>
2022-06-04 06:13:37 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-
+/**
+ *main - main entry
+ *print the message
+ *Return: Always 0(success)
+ **/
 int main()
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
-
-
-  
