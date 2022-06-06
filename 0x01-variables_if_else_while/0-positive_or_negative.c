8a5be68b04af9824e4bc04b879f6ae2f967510f9
Aletia88 <abababmgmgm@gmail.com>
2022-06-06 12:16:18 -0700

    if else

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,25 +8,21 @@
  **/
 int main()
 {
-  int num;
-
-  /* Input number from user */
-  printf("Enter any number: ");
-  scanf("%d", &num);
-
-
-  if(num > 0)
+int num;
+printf("Enter any number: ");
+scanf("%d", &num);
+if (num > 0)
     {
       printf("%d is postive\n",num);
     }
-  if(num < 0)
+if(num < 0)
     {
       printf("%d is negative\n", num);
     }
-  if(num == 0)
+if(num == 0)
     {
       printf("%d is zero\n",num);
     }
 
-  return 0;
+return 0;
 }
