0527d379c24e8dde9f9977e3ea077d3b51d10473
Aletia88 <abababmgmgm@gmail.com>
2022-06-06 20:23:12 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- *main - main entry 
+ *main - main entry
  *Return: Always 1(success)
- **/
+ */
 int main(void)
 {
 char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
