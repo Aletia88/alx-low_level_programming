86989da3f749c7649ff11476bd1bf68947810dce
Aletia88 <abababmgmgm@gmail.com>
2022-06-06 19:57:44 -0700

    compiler

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,11 +3,10 @@
 #include <stdlib.h>
  /**
   * main - entry point
-  * main - main entry
   * Description: prints all single digit numbers of base 10
   * starting from 0, followed by a new line.
   * Return: 0
-  */
+  **/
 int main(void)
 {
 char d = '0';
