15830547b36515bf78b1980c0bec02c17442c17b
Aletia88 <abababmgmgm@gmail.com>
2022-06-07 21:34:25 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *main - main entry
+ *Return: 0
+ */
+int main(void)
+{
+int i;
+char a[] = "_putchar";
+for (i = 0; i < 8; i++)
+{
+_putchar(a[i]);
+}
+_putchar('\n');
+return(0);
+}
