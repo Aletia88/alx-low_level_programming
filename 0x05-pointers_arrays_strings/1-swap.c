8ee93aa49052b76f62db1a52273e59b1f0b6ddd8
Aletia88 <abababmgmgm@gmail.com>
2022-06-13 12:51:32 -0700

    strlen

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,7 +2,7 @@
 /**
  * swap_int -  swaps the values of two integers
  *@a: int value of a ponter
- *@b pointer of int value
+ *@b: pointer of int value
  *Return: Always 0
  */
 void swap_int(int *a, int *b)
