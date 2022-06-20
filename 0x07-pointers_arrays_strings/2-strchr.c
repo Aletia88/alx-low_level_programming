6b8ca8861de7c82b85f9922ed7cda40acb3687c2
Samrawit Girma <abababmgmgmg@gmail.com>
2022-06-20 15:00:09 -0700

    string length

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,17 +8,17 @@
  */
 char *_strchr(char *s, char c)
 {
-	int i;
-	while (1)
-	{
-		i = *s++;
-		if (i == c)
-		{
-			return (s - 1);
-		}
-		if (i == 0)
-		{
-			return (NULL);
-		}
-	}
+int i;
+while (1)
+{
+i = *s++;
+if (i == c)
+{
+return (s - 1);
+}
+if (i == 0)
+{
+return (NULL);
+}
+}
 }
