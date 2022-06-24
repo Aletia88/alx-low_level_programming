f693c95fadfdb0fae56c10be6e4c51263a885e64
Samrawit Girma <abababmgmgmg@gmail.com>
2022-06-24 12:40:34 -0700

    args

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,16 +1,15 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
- * main - a program that prints the number of arguments passed into it
- * @argc: argument count
- * @argv: argument vector
- * Return: Always 0
+ * main - A program that prints the number of arguements you passed into it
+ * @argc: An argument counter
+ * @argv: An argument values
+ * Return: Always 0.
  */
 int main(int argc, char *argv[])
 {
 if (argv[0])
-{
-printf("%d/n", argc - 1);
+printf("%d\n", argc - 1);
 return (0);
-}
 }
